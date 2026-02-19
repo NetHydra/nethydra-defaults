09c5b18e629888922d942e656d79fdb5c00eaf1e
me-joe <ctf.beginer.joe@gmail.com>
2026-02-19 08:29:02 -0500

    Update: print status when triggered

--- share/bin/helper-disable-service.sh
+++ share/bin/helper-disable-service.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "Disable unwanted service..."
 for service in $(cat /usr/share/hydrapwk-defaults/service-to-disable); do
 	/usr/bin/systemctl --quiet disable "$service" >/dev/null 2>&1 || true
 	/usr/bin/systemctl --quiet stop "$service" >/dev/null 2>&1 || true
